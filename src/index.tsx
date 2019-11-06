a9f52f15853b28b6783423a850e4dcf630c4912c
Maxim Boichenko <maxim.boichenko@gmail.com>
2019-11-06 18:52:44 +0300

    add ie 11 support

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,8 @@
+// Импорт 'react-app-polyfill/ie11' для  поддержки ie 11 в create react app 3
+// Этот импорт должен идти первым в index.tsx
+// (https://github.com/facebook/create-react-app/blob/master/packages/react-app-polyfill/README.md#internet-explorer-11)
+import 'react-app-polyfill/ie11';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
