dc228d4338e8e84dfd9e21c73fa0eec0917accb5
Maxim Boichenko <maxim.boichenko@gmail.com>
2019-11-07 08:14:23 +0300

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import './App.css';
+import { Questions } from './types';
 
-const App: React.FC = () => {
-  return <h1>Hello</h1>;
-};
+interface AppState {
+  questions: Questions | null;
+}
+
+class App extends React.Component<{}, AppState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = { questions: null };
+  }
+
+  public render(): ReactNode {
+    return (
+      <>
+        {!this.state.questions && (
+          <div className="screenCenter">Loading...</div>
+        )}
+        {this.state.questions && <div className="container">Hello</div>}
+      </>
+    );
+  }
+}
 
 export default App;
