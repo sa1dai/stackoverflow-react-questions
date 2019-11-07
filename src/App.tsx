cc3050b35daa41f5e996775bb0f3e1db05e9dcec
Maxim Boichenko <maxim.boichenko@gmail.com>
2019-11-07 08:31:03 +0300

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
 import React, { ReactNode } from 'react';
 import './App.css';
 import { Questions } from './types';
+import StackoverflowApiService from './StackoverflowApiService';
 
 interface AppState {
   questions: Questions | null;
+  error: string | null;
 }
 
 class App extends React.Component<{}, AppState> {
   constructor(props: {}) {
     super(props);
-    this.state = { questions: null };
+    this.state = { questions: null, error: null };
+  }
+
+  async componentDidMount(): Promise<void> {
+    const service = new StackoverflowApiService();
+    service
+      .getReactQuestions()
+      .then(questions => this.setState({ questions }))
+      .catch(() => this.setState({ error: 'Error occurred!' }));
   }
 
   public render(): ReactNode {
+    const { questions, error } = this.state;
+    if (error) {
+      return <div className="screenCenter">{error}</div>;
+    }
     return (
       <>
-        {!this.state.questions && (
-          <div className="screenCenter">Loading...</div>
-        )}
-        {this.state.questions && <div className="container">Hello</div>}
+        {!questions && <div className="screenCenter">Loading...</div>}
+        {questions && <div className="container">Hello</div>}
       </>
     );
   }
