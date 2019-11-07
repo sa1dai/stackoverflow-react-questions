bdfb788ad20004707d439d76c4f45250e8f27c68
Maxim Boichenko <maxim.boichenko@gmail.com>
2019-11-07 20:11:13 +0300

    polyfill for ie 11

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@
 // Этот импорт должен идти первым в index.tsx
 // (https://github.com/facebook/create-react-app/blob/master/packages/react-app-polyfill/README.md#internet-explorer-11)
 import 'react-app-polyfill/ie11';
+// Полифилл для старых браузеров
+// https://github.com/facebook/create-react-app/blob/master/packages/react-app-polyfill/README.md#polyfilling-other-language-features
+import 'react-app-polyfill/stable';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
